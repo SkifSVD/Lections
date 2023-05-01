38ccd3c6da9b4ea3a4c28703f3de3173af76e559
SkifSVD <57569473+SkifSVD@users.noreply.github.com>
2023-05-01 20:53:31 +0300

    Add files via upload

---
+++ classwork_26.04.23/task55.py
@@ -0,0 +1,78 @@
+# #todo: Написать авторизацию пользователя в систему.
+# При реализации авторизации спроектировать абстрактный класс и реализовать методы в наследуемом классе
+# login, check_password, check_login
+
+# При запуске программы пользователю необходимо ввести
+# логин и пароль, поэтапно.
+# login: _________
+# password: ________
+#
+# При неправильном вводе логина должно генерироваться пользовательское исключение LoginNotFound
+# Введеный пароль должен проверятся на длину. Длина должна быть более 8 символов иначе генерируем пользовательское
+# исключение LengthError
+# При вводе некорректного пароля генерируем соответсвуещее исключение
+# При успешном заходе генерируем исключение "Доступ разрешен!"
+
+from abc import ABC, abstractmethod
+
+class Users(ABC):
+    @abstractmethod
+    def login(self):
+        pass
+
+    def check_pasword(self):
+        pass
+
+    def check_login(self):
+        pass
+
+class User(Users):
+    users = {"User":"Password", "Skif":"umbalumba"}
+    def login(self, l, p):
+        try:
+            self.check_login(l) #*
+            self.check_pasword(p)#*
+            if l in self.users and p == self.users[l]:
+                print("Доступ Разрешен")
+            else:
+                raise LoginError
+        except LoginError:
+            pass
+
+    def check_pasword(self, p):
+        try:
+             raise LengthError(len(p))
+        except LengthError:
+            pass
+
+        
+
+    def check_login(self, login):
+        try:
+            if not login in self.users:
+                raise LoginNotFound
+        except LoginNotFound:
+            pass
+
+class LengthError(Exception):
+    def __init__(self, number):
+        if number < 8:
+            print(f"В пароле должно быть не менее 8 символов! сейчас: {number}")
+
+class LoginNotFound(Exception):
+    def __init__(self):
+        print(f"Логин не найден!")
+
+class LoginError(Exception):
+    def __init__(self):
+        print("Доступ запрещён!")
+
+        
+
+
+user = User()
+while True:
+    login = input("Введите логин: ")
+    password = input("Введите пароль: ")
+
+    user.login(login, password)
---
+++ classwork_26.04.23/task56.py
@@ -0,0 +1,47 @@
+# todo: Создать абстрактный класс Press (пресса) содержащий:
+# Поля: название, цена за единицу.
+# В классе должны быть абстрактные методы:
+# метод SetPrice (без параметров) – установка цены.
+# Метод Info - информация (без параметров), который возвращает строку, содержащую информацию об объекте.
+#
+# На его основе реализовать дочерние классы:
+# Magazine - журнал,
+# Book- книга.
+
+from abc import ABC, abstractmethod
+from selectors import DefaultSelector
+
+class Press(ABC):
+    name = ""
+    price = 0
+
+    @abstractmethod
+    def SetPrice(self):
+        pass
+
+    def Info(self):
+        return f"Название: {self.name}, Стоимость: {self.price} руб."
+
+class Magazine(Press):
+    def __init__(self, name):
+        self.name = name
+
+    def SetPrice(self, price):
+        self.price = price
+
+class Book(Press):
+    def __init__(self, name):
+        self.name = name
+
+    def SetPrice(self, price):
+        self.price = price
+        
+
+
+m1 = Magazine("Сад и огород")
+m1.SetPrice(128.50)
+
+b1 = Book("Мастер и Маргарита")
+b1.SetPrice(1230.43)
+print(m1.Info())
+print(b1.Info())
